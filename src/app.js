92825cbd679b52e2bdb1372844da1b58e85626fe
C3-SantoshSantosh-c <santosh.santosh-c@c3.ai>
2025-09-25 09:11:24 +0530

    Rquest handler function

--- src/app.js
+++ src/app.js
@@ -3,8 +3,11 @@ const express = require("express");
 const app = express();
 
 // Request handler function
-app.use((req, res) => {
-  res.send("Hello!");
+app.use("/", (req, res) => {
+  res.send("Home Page!");
+});
+app.use("/dashboard", (req, res) => {
+  res.send("Dashboard Page!");
 });
 
 app.listen(7777);
